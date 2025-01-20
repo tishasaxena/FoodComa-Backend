241577322e4f48901d60e083629836da4e969206
Tisha Saxena <tishasaxena0@gmail.com>
2025-01-20 17:05:23 +0530

    initial code

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const ServerConfig = require('./config/serverConfig');
+
+const app = express();
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Server started at port ${ServerConfig.PORT} ..`);
+    
+
+})
+
