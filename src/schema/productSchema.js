5dc9f014d1a56a3f3a4a533c83699b1387b5df65
Tisha Saxena <149892697+tishasaxena@users.noreply.github.com>
2025-04-12 01:04:06 +0530

    clodinary configuration

---
+++ src/schema/productSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: [true, "Product name is required"],
+        minLength: [5, "Product name must be atleast 5 characters long"],
+        trim: true
+    },
+    description: {
+        type: String,
+        minLength: [5, "Product description must be atleast 5 characters long"]
+    },
+    productImage: {
+        type: String
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"]
+    },
+    category: {
+        type: String,
+        enum: ['veg', 'non-veg', 'drinks', 'sides'],
+        default: 'veg'
+    },
+    inStock: {
+        type: Boolean,
+        required: [true, "In Stock status is required"],
+        default: true
+    }
+}, {
+
+    timestamps: true       
+
+});
+
+const Product = mongoose.model("Product", productSchema);  
+
+
+module.exports = Product;  
