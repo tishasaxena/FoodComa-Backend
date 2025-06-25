d288dbe460f8222b86c649cdab48526ddf03904a
Tisha Saxena <149892697+tishasaxena@users.noreply.github.com>
2025-06-25 19:09:59 +0530

    Added order api

--- src/schema/orderSchema.js
+++ src/schema/orderSchema.js
@@ -41,6 +41,7 @@ const orderSchema = new mongoose.Schema({
     paymentMethod: {
       type: String,
       enum: ["ONLINE", "CASH"],
+
       default: "CASH"
     }
 },   
--- src/utils/badRequestError.js
+++ src/utils/badRequestError.js
@@ -1,15 +1,14 @@
-
-
-const AppError = require('./appError');
+const AppError = require("./appError");
 
 class BadRequestError extends AppError {
-  constructor(invalidParams) {
-    // invalidParams is expected to be an array of strings
-    const message = `The request has the following invalid parameters:\n` + 
-                    invalidParams.map(param => `- ${param}`).join('\n');
+    constructor(invalidParams) {
+        // invalidParams: []
 
-    super(message, 400);  // 400 is the HTTP status code for Bad Request
-  }
+        let message = "";
+        invalidParams.forEach(params => message += `${params}\n`);
+        console.log(message)
+        super(`The request has the following invalid parameters \n${invalidParams}`, 400);
+    }
 }
 
 module.exports = BadRequestError;
