f9e166ddd8f044d70fba814cabebc998e8f41580
Tisha Saxena <149892697+tishasaxena@users.noreply.github.com>
2025-07-07 00:22:46 +0530

    Modified

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,65 +1,60 @@
-const express = require("express");
+const { COOKIE_SECURE, FRONTEND_URL } = require("../config/serverConfig");
 const { loginUser } = require("../services/authService");
+
 
 async function logout(req, res) {
 
     console.log("Cookie from frontend", req.cookies);
 
     res.cookie("authToken", "", {
-      httpOnly: true,
-        secure: false, 
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    });
-    return res.status(200).json({
-        success: true,
-        message: "Logout successful",
-        data: {},
-        error: {}
-    });
-
-
-}
-
-async function login(req, res) {
-
-   try{
-    const loginPayload = req.body;
-
-    const response = await loginUser(loginPayload);
-
-    res.cookie("authToken", response.token, {
         httpOnly: true,
-        secure: false, 
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    }); 
-
+        secure: COOKIE_SECURE,
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: FRONTEND_URL
+    });
     return res.status(200).json({
         success: true,
-        message:  'Logged in successfully',
-        data: {
-            userRole: response.userRole,
-            userData: response.userData 
-        },
-        error: {}
-       })
+        message: "Log out successfull",
+        error: {},
+        data: {}
+    });
+}
+async function login(req, res) {
+    
+    try {
+        const loginPayload = req.body;
 
-    }
+        const response = await loginUser(loginPayload);
 
-    catch(error){
+        res.cookie("authToken", response.token, {
+            httpOnly: true,
+            secure: COOKIE_SECURE,
+            sameSite: "lax",
+            domain: FRONTEND_URL,
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: 'Logged in successfully',
+            data: {
+                userRole: response.userRole,
+                userData: response.userData
+            },
+            error: {}
+        })
+    } catch(error) {
         return res.status(error.statusCode).json({
             success: false,
-            data:{},
-            message: error.message  ,
+            data: {},
+            message: error.message,
             error: error
         })
-            
     }
 
 }
-    
-module.exports = {
-        login,
-        logout
 
-    
-    };
+module.exports = {
+    login,logout
+}
